36864fc21b6d8c8fe68846741884ad658f9318c2
Tkatchuk <c775tech@yahoo.com>
2023-08-27 00:00:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -101,3 +101,54 @@ function selectDay(index) {
 
     eventsDisplay.innerHTML = htmlContent;
 }
+
+
+const speakersData = [
+    {
+        day: 1,
+        image: 'path/to/image1.jpg', 
+        name: 'John Doe',
+        role: 'CEO at ABC Corp',
+        area: 'Strategic Decision Making',
+        additionalInfo: 'John has over 20 years of experience in...'
+    },
+    {
+        day: 2,
+        image: 'path/to/image1.jpg', 
+        name: 'joe Doe',
+        role: 'CEO at ABC Corp',
+        area: 'Strategic Decision Making',
+        additionalInfo: 'John has over 20 years of experience in...'
+    },
+    {
+        day: 2,
+        image: 'path/to/image1.jpg', 
+        name: 'andrew Doe',
+        role: 'CEO at ABC Corp',
+        area: 'Strategic Decision Making',
+        additionalInfo: 'John has over 20 years of experience in...'
+    }
+    // ... Add more speakers for each day similarly
+];
+
+function displaySpeakersForDay(day) {
+    const speakers = speakersData.filter(s => s.day === day);
+    const speakerContainer = document.getElementById('speakerContainer');
+
+    let htmlContent = '';
+    speakers.forEach(speaker => {
+        htmlContent += `
+            <div class="speaker-card">
+                <img src="${speaker.image}" alt="${speaker.name}">
+                <div class="speaker-info">
+                    <h3>${speaker.name}</h3>
+                    <p><strong>Role:</strong> ${speaker.role}</p>
+                    <p><strong>Area:</strong> ${speaker.area}</p>
+                    <p>${speaker.additionalInfo}</p>
+                </div>
+            </div>
+        `;
+    });
+
+    speakerContainer.innerHTML = htmlContent;
+}
