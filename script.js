e1474273202925bec7faab9ece2451ce4bbb9bce
Tkatchuk <c775tech@yahoo.com>
2023-09-08 23:46:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,117 +1,23 @@
-function loadContent(contentType) {
-    const contentDiv = document.getElementById('content');
-
-    fetch(`${contentType}.html`)
-        .then(response => response.text())
-        .then(data => {
-            contentDiv.innerHTML = data;
-        })
-        .catch(error => {
-            console.error("Error fetching content:", error);
-            contentDiv.innerHTML = '<p>Error loading content.</p>';
-        });
+function loadContent(url) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            document.getElementById('contentContainer').innerHTML = this.responseText;
+        }
+    };
+    xhr.send();
 }
-
-function updateCountdown() {
-    const now = new Date();
-    const targetDate = new Date(now.getFullYear(), 11, 12);  // December is month 11 in JavaScript (0-indexed)
-    const timeDiff = targetDate - now; // Difference in milliseconds
-    
-    // Calculate days, hours, minutes, seconds
-    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
-
-    // Display the countdown
-    document.querySelector('#countdown span').textContent = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-}
-
-
-
-//js for calender
-
-const eventData = [
-    [ // Day 1: Welcome Day
-        {time: "09:00 AM", event: "Welcome Breakfast"},
-        {time: "10:00 AM", event: "Orientation & Introduction to Management Symposium"},
-        {time: "12:00 PM", event: "Lunch at Venue"},
-        {time: "01:00 PM - 05:00 PM", event: "Free Time for Exploring the Area"}
-    ],
-    [ // Day 2: Foundations of Management
-        {time: "09:00 AM", event: "Opening Remarks"},
-        {time: "10:00 AM", event: "Speaker 1: Strategic Decision Making"},
-        {time: "11:30 AM", event: "Coffee Break"},
-        {time: "12:00 PM", event: "Speaker 2: Navigating Organizational Culture"},
-        {time: "01:00 PM", event: "Lunch Break"},
-        {time: "02:30 PM", event: "Speaker 3: Effective Team Management"},
-        {time: "04:00 PM", event: "Panel Discussion: Trends in Management"}
-    ],
-    [ // Day 3: Advanced Management Techniques
-        {time: "09:00 AM", event: "Recap of Day 2"},
-        {time: "10:00 AM", event: "Speaker 4: The Role of Data in Decision Making"},
-        {time: "11:30 AM", event: "Coffee Break"},
-        {time: "12:00 PM", event: "Speaker 5: Change Management in Dynamic Industries"},
-        {time: "01:00 PM", event: "Lunch Break"},
-        {time: "02:30 PM", event: "Speaker 6: Leadership vs Management: Striking the Balance"},
-        {time: "04:00 PM", event: "Closing Remarks & Preview of Workshops"}
-    ],
-    [ // Day 4: Workshops and Practical Applications
-        {time: "09:00 AM", event: "Workshop: Real-world Strategic Planning"},
-        {time: "11:00 AM", event: "Coffee Break"},
-        {time: "11:30 AM", event: "Workshop: Navigating Corporate Politics and Power Dynamics"},
-        {time: "01:00 PM", event: "Lunch & Networking Session"},
-        {time: "03:00 PM", event: "Interactive Session: Role-playing in Crisis Management"},
-        {time: "05:00 PM", event: "Farewell and Commitment to Continuous Learning"}
-    ]
-];
-
-// The selectDay function remains unchanged.
-
-
-function selectDay(index) {
-    const events = eventData[index];
-    const eventsDisplay = document.getElementById('eventsDisplay');
-
-    let htmlContent = '';
-    events.forEach(event => {
-        htmlContent += `
-            <div class="eventItem">
-                <strong>${event.time}</strong>: ${event.event}
-            </div>
-        `;
-    });
-
-    eventsDisplay.innerHTML = htmlContent;
-}
-
 
 const speakersData = [
     {
-        day: 1,
-        image: 'headshots/andrew-cherng.jpg', , 
-        name: 'Andrew cherng',
-        role: 'CEO',
-        area: 'Panda-RG',
-        additionalInfo: 'rem ipsum dolor sit amet, consectetur adipiscing elit.'
-    },    
-	{
         day: 2,
-        image: 'headshots/j-wang.jpg',  
-        name: 'jeff wang',
-        role: 'ZVP',
-        area: 'T-Bone',
-        additionalInfo: 'rem ipsum dolor sit amet, consectetur adipiscing elit. '
-    },    
-	{
-        day: 3,
-        image: 'headshots/p-cherng2.jpg', 
-        name: 'peggy cherng',
-        role: 'COO',
-        area: 'Panda-RG',
-        additionalInfo: 'rem ipsum dolor sit amet, consectetur adipiscing elit. '
+        image: 'https://via.placeholder.com/150', 
+        name: 'John Doe',
+        role: 'CEO at ABC Corp',
+        area: 'Strategic Decision Making',
+        additionalInfo: 'John has over 20 years of experience...'
     },
-
     // ... Add more speakers for each day similarly
 ];
 
@@ -135,4 +41,8 @@ function displaySpeakersForDay(day) {
     });
 
     speakerContainer.innerHTML = htmlContent;
+}
+
+window.onload = function() {
+    displaySpeakersForDay(2);  // Load Day 2 speakers for testing on page load.
 }
